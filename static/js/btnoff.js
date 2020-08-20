f312876c100a09b88964422d4f244624b1848918
Nagisa Ashihara <nemuco.nemu@gmail.com>
2020-08-20 20:40:24 +0900

    閉じるボタン色変更完了、（menua.html）

---
+++ static/js/btnoff.js
@@ -0,0 +1,7 @@
+$(function () {
+    // 「#hidebtn」要素に対するclickイベントを作成してください
+    $('#hidebtn').click(function () {
+        $('#btn_eat').css('background-color', '#A9A9B4');
+        $('#btn_muscle').css('background-color', '#A9A9B4');
+    })
+});
